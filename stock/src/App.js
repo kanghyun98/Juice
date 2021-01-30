2404b46dfd0a871dfb71da84c5b37244f49a63b8
jhy979 <jhy979@gmail.com>
2021-01-31 00:45:53 +0900

    주식 알람 서비스 첫 웹페이지 레이아웃
    팀원들과 상의한 웹페이지의 메인페이지 디자인입니다.
    이런 레이아웃으로 앞으로 케러세을 추가할 예정입니다.

---
+++ stock/src/App.js
@@ -0,0 +1,53 @@
+/*eslint-disalbe*/
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Nav,Navbar,NavDropdown,Button,Form,FormControl,Jumbotron,Col,Row,Container} from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+     <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Navbar>
+      <Jumbotron>
+        <h1>Hello, world!</h1>
+        <p>
+          This is a simple hero unit, a simple jumbotron-style component for calling
+          extra attention to featured content or information.
+        </p>
+        <p>
+          <Button variant="primary">Learn more</Button>
+        </p>
+      </Jumbotron>
+
+      <Container>
+        <Row>
+          <Col>1 of 3</Col>
+          <Col xs={5}>2 of 3 (wider)</Col>
+          <Col>3 of 3</Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
