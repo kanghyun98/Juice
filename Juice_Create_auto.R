cfd8bd2dc71110b63f4f6bdf78e9c6d2bc0315da
20170781 <lkh981107@naver.com>
2021-03-16 23:33:31 +0900

    파일위치 변경에 따른 파일코드 변경

--- Juice_Create_auto.R
+++ Juice_Create_auto.R
@@ -3,14 +3,14 @@ library(taskscheduleR)
 library(KeyboardSimulator)
 
 
-taskscheduler_create(taskname="Update_for_Stockprice_auto", rscript="C:/Stock_project/Juice_Update_auto.R" #16:30
+taskscheduler_create(taskname="Update_Stockprice_auto", rscript="C:/Juice_Stock_project/Juice_Update_auto.R" #16:30
                      ,days=c("MON", "TUE","WED", "THU", "FRI"), schedule="WEEKLY"
                      ,starttime="16:30",startdate=format(Sys.Date(),"%Y/%m/%d"))
 
-taskscheduler_create(taskname="Update_for_News_auto", rscript="C:/Stock_project/Juice_Update_auto.R"  #23:00
+taskscheduler_create(taskname="Update_News_auto", rscript="C:/Juice_Stock_project/Juice_Update_auto.R"  #23:00
                      ,days=c("MON", "TUE","WED", "THU", "FRI"), schedule="WEEKLY"
                      ,starttime="23:00",startdate=format(Sys.Date(),"%Y/%m/%d"))
 
 taskscheduler_ls()              #예약확인
 
-taskscheduler_delete(taskname="test_auto")  #예약취소 
+taskscheduler_delete(taskname="~")  #예약취소 
